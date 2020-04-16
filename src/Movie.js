1bedbc52312074f347473dbc7f589f572465ed87
elbum <elbum@hanmail.net>
2020-04-16 12:18:10 +0900

    desktop

--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
-    return <div class="movie">
+function Movie({ id, year, title, summary, poster, genres }) {
+    return <div className="movie">
         <img src={poster} alt={title} title={title}></img>
-        <div class="movie__data">
-            <h3 class="movie__title" style={{ backgroundColor: "red" }}>{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie__summary">{summary}</p>
+        <div className="movie__data">
+            <h3 className="movie__title" style={{ backgroundColor: "red" }}>{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <p className="movie__summary">{summary}</p>
         </div>
     </div>
 };
@@ -18,7 +18,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
